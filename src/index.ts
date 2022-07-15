e65d409b2adca1fa718f1f7629de5c0b0ec8fd9c
Andrej Jurkin <andrej@jurkin.io>
2022-07-15 20:54:04 +0200

    Hello world

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,40 @@
 import { readInput } from "./cli/readInput";
 import createProject from "./output/createProject";
+import getPackageManager from "./utils/getPackageManager";
 import { logger } from "./utils/logger";
+import chalk from "chalk";
+import path from "path";
+import ora from "ora";
+import fs from "fs-extra";
+import getTemplateDir from "./utils/getTemplateDir.js";
+
+const wait = async (seconds: number) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, seconds);
+  });
+};
 
 async function main() {
   logger.info("Welcome to create-tailwind-app!");
 
   const input = await readInput();
-  logger.success(`User input: ${JSON.stringify(input, null, 2)}`);
-  createProject(input);
+  const pkgManager = getPackageManager();
+  const projectDir = path.resolve(process.cwd(), input.appName);
+  const templateDir = getTemplateDir(input);
+
+  logger.info(`\nUsing: ${chalk.cyan.bold(pkgManager)}\n`);
+
+  const spinner = ora(`Scaffolding project in ${projectDir}`).start();
+
+  await fs.copy(templateDir, projectDir);
+
+  spinner.succeed(
+    `Finished scaffolding ${chalk.green.bold(input.appName)} project base.`,
+  );
+
+  // createProject(input);
 
   process.exit(0);
 }
