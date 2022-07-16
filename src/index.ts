812a141ed28ccb381aaf7dadc8e035b0beb5febc
Andrej Jurkin <andrej@jurkin.io>
2022-07-16 21:21:10 +0200

    Scaffold vite app & read its package.json test

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,7 @@ import path from "path";
 import ora from "ora";
 import fs from "fs-extra";
 import getTemplateDir from "./utils/getTemplateDir.js";
-
-const wait = async (seconds: number) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(true);
-    }, seconds);
-  });
-};
+import execAsync from "./utils/execAsync";
 
 async function main() {
   logger.info("Welcome to create-tailwind-app!");
@@ -26,15 +19,26 @@ async function main() {
 
   logger.info(`\nUsing: ${chalk.cyan.bold(pkgManager)}\n`);
 
+  // Copy the template to the project directory
   const spinner = ora(`Scaffolding project in ${projectDir}`).start();
+  // TODO: Check if exists and ask to overwrite
+  // If the project directory exists, remove it
+  if (fs.existsSync(projectDir)) {
+    fs.removeSync(projectDir);
+  }
 
-  await fs.copy(templateDir, projectDir);
+  await fs.mkdir(projectDir);
+  await execAsync(
+    `yarn create vite ${input.appName as string} --template vanilla`,
+  );
 
   spinner.succeed(
     `Finished scaffolding ${chalk.green.bold(input.appName)} project base.`,
   );
 
-  // createProject(input);
+  // Get package.json
+  const packageJson = await fs.readJSON(path.join(projectDir, "package.json"));
+  console.log("packageJson", packageJson);
 
   process.exit(0);
 }
