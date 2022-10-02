13d14e0f957dc393eb44c4be7de1048ad3c4d648
Philipp Dormann <philipp@philippdormann.de>
2022-10-02 23:19:01 +0200

    updated utils/getPackageManager.ts (inspired by create-next-app)

--- src/utils/getPackageManager.ts
+++ src/utils/getPackageManager.ts
@@ -1,35 +1,25 @@
-export type PackageManager = "npm" | "yarn" | "pnpm";
+import { execSync } from 'child_process'
 
-/**
- * @link https://stackoverflow.com/a/69301988
- * 
- * @returns best guess at user's package manager
- */
-export default function getPackageManager() {
-  // This environment variable is set by npm and yarn but pnpm seems less consistent
-  const agent = process.env.npm_config_user_agent;
+export type PackageManager = 'npm' | 'pnpm' | 'yarn'
 
-  if (!agent) {
-      // This environment variable is set on Linux but I'm not sure about other OSes.
-      const parent = process.env._;
-
-      if (!parent) {
-          // No luck, assume npm
-          return "npm";
+export default function getPackageManager(): PackageManager {
+  try {
+    const userAgent = process.env.npm_config_user_agent
+    if (userAgent) {
+      if (userAgent.startsWith('yarn')) {
+        return 'yarn'
+      } else if (userAgent.startsWith('pnpm')) {
+        return 'pnpm'
       }
-
-      if (parent.endsWith("pnpx") || parent.endsWith("pnpm")) return "pnpm";
-      if (parent.endsWith("yarn")) return "yarn";
-
-      // Assume npm for anything else
-      return "npm";
+    }
+    try {
+      execSync('yarn --version', { stdio: 'ignore' })
+      return 'yarn'
+    } catch {
+      execSync('pnpm --version', { stdio: 'ignore' })
+      return 'pnpm'
+    }
+  } catch {
+    return 'npm'
   }
-
-  const [program] = agent.split("/");
-
-  if (program === "yarn") return "yarn";
-  if (program === "pnpm") return "pnpm";
-
-  // Assume npm
-  return "npm";
-};
+}
