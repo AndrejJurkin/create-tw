3f52946b656ddd84031e94b578cea551b165043a
Philipp Dormann <philipp@philippdormann.de>
2022-10-02 23:22:13 +0200

    format utils/getPackageManager.ts

--- src/utils/getPackageManager.ts
+++ src/utils/getPackageManager.ts
@@ -1,25 +1,25 @@
-import { execSync } from 'child_process'
+import { execSync } from "child_process";
 
-export type PackageManager = 'npm' | 'pnpm' | 'yarn'
+export type PackageManager = "npm" | "pnpm" | "yarn";
 
 export default function getPackageManager(): PackageManager {
   try {
-    const userAgent = process.env.npm_config_user_agent
+    const userAgent = process.env.npm_config_user_agent;
     if (userAgent) {
-      if (userAgent.startsWith('yarn')) {
-        return 'yarn'
-      } else if (userAgent.startsWith('pnpm')) {
-        return 'pnpm'
+      if (userAgent.startsWith("yarn")) {
+        return "yarn";
+      } else if (userAgent.startsWith("pnpm")) {
+        return "pnpm";
       }
     }
     try {
-      execSync('yarn --version', { stdio: 'ignore' })
-      return 'yarn'
+      execSync("yarn --version", { stdio: "ignore" });
+      return "yarn";
     } catch {
-      execSync('pnpm --version', { stdio: 'ignore' })
-      return 'pnpm'
+      execSync("pnpm --version", { stdio: "ignore" });
+      return "pnpm";
     }
   } catch {
-    return 'npm'
+    return "npm";
   }
 }
