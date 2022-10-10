062ad850398720064dbe4c9464429802d075db4c
Andrej Jurkin <andrej@jurkin.io>
2022-10-10 21:45:19 +0200

    Update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -98,10 +98,11 @@ const config: PlaywrightTestConfig = {
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: {
+    command: "cd dist/nextjs && yarn dev",
+    port: 3000,
+    reuseExistingServer: !process.env.CI,
+  },
 };
 
 export default config;
